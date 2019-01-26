8b38c4d4d5ee2a4a503c2d596cf235f3c1053bc0
futamidaiki <nimi0370@gmail.com>
2019-01-27 03:49:44 +0900

    ADD dockerfile and FIX main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 import io
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input
-
+from keras import backend as K
 
 app = Flask(__name__)
 #Scss(app, static_dir='static', asset_dir='assets')
@@ -35,27 +35,20 @@ def predict():
         img = request.files['picfile'].read()
         img = Image.open(io.BytesIO(img))
         img.save('test.jpg')
-
         img = np.asarray(img) / 255.
         img = np.expand_dims(img, axis=0)
+            #K.clear_session()
         pred = model.predict(img)
-
-
-        pred = model.predict(img)
-
+            # pred = model.predict(img)
         disease = [
-            'ごま葉枯病',
-            'いもち病',
-            '縞葉枯病',
-        ]
-
+                'ごま葉枯病',
+                'いもち病',
+                '縞葉枯病',
+                ]
         confidence = str(round(max(pred[0]), 3))
         pred = disease[np.argmax(pred)]
-
-
         data = dict(pred=pred, confidence=confidence)
         return jsonify(data)
-
     return 'Picture info did not get saved.'
 
 
@@ -67,6 +60,7 @@ def current_image():
 
 
 if __name__ == '__main__':
+    K.clear_session()
     load_model()
-    # model._make_predict_function()
-    app.run(debug=False, port=5000)
+    model._make_predict_function()
+    app.run(debug=True,host="0.0.0.0",port=5000)
